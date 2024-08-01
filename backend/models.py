ff7ccf4a0cf995fcb2d3bc46a8d85f24a0c0ab1f
toyman640 <falakoadebayor@gmail.com>
2024-08-01 15:39:50 +0100

    Add picture to user model

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class CustomerUser(AbstractUser):
   phone_number = models.CharField(max_length=15, blank=True, null=True)
   means_of_id = models.FileField(blank=True)
+  avartar = models.ImageField(blank=True)
+
 
   def __str__(self):
     return self.username
