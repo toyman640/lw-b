5253633b98b0ed02b1a32cac4b7f8c31120e0b1f
toyman640 <falakoadebayor@gmail.com>
2024-08-01 14:27:18 +0100

    Make first migration

--- lw_b/settings.py
+++ lw_b/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders'
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
