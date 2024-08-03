56b3630a5a63cc0148c7bfb63bbced2ed6476bd0
toyman640 <falakoadebayor@gmail.com>
2024-08-03 14:47:17 +0100

    User creation logic added succesfully

--- backend/serializers.py
+++ backend/serializers.py
@@ -4,5 +4,5 @@ from .models import CustomerUser
 class CustomerUserSerializer(serializers.ModelSerializer):
   class Meta:
     model = CustomerUser
-    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'means_of_id', 'avatar', 'is_active']
+    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'means_of_id', 'avartar', 'is_active']
     read_only_fields = ['is_active']
