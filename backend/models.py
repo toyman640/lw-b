b1c2bf147d0c9e1ca61a6a320bab41d420c33875
toyman640 <falakoadebayor@gmail.com>
2024-08-03 15:09:02 +0100

    Add country and state to user

--- backend/models.py
+++ backend/models.py
@@ -7,6 +7,8 @@ class CustomerUser(AbstractUser):
   phone_number = models.CharField(max_length=15, blank=True, null=True)
   means_of_id = models.FileField(blank=True)
   avartar = models.ImageField(upload_to='avatars/', blank=True)
+  countrty = models.CharField(max_length=10, blank=True, null=True)
+  state = models.CharField(max_length=10, blank=True, null=True)
 
 
   def __str__(self):
