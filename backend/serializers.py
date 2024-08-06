b0e495ad704d13441dce8ae5c4941ff39fc70d54
toyman640 <falakoadebayor@gmail.com>
2024-08-06 16:54:10 +0100

    Add property and image models, serializers, views and urls

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomerUser, Category
+from .models import CustomerUser, Category, Property, PropertyImage
 
 class CustomerUserSerializer(serializers.ModelSerializer):
   class Meta:
@@ -11,3 +11,18 @@ class CategorySerializer(serializers.ModelSerializer):
   class Meta:
     model = Category
     fields = ['id', 'name']
+
+
+class PropertyImageSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = PropertyImage
+    fields = ['image', 'description']
+
+class PropertySerializer(serializers.ModelSerializer):
+  images = PropertyImageSerializer(many=True, read_only=True)
+  posted_by = serializers.StringRelatedField(read_only=True)
+
+  class Meta:
+    model = Property
+    fields = ['id', 'title', 'price', 'country', 'state', 'area', 'category', 'description', 'posted_by', 'approved', 'images']
+    read_only_fields = ['approved']
--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import current_user_view, CustomerUserListView, CustomerUserCreateView, CustomerUserDetailView, LoginView, LogoutView, CategoryListView, CategoryCreateView
+from .views import (current_user_view, 
+                    CustomerUserListView, 
+                    CustomerUserCreateView, 
+                    CustomerUserDetailView, 
+                    LoginView, LogoutView, 
+                    CategoryListView, CategoryCreateView, PropertyListView, ApprovedPropertyListView, PropertyCreateView, PropertyDetailView)
 
 urlpatterns = [
   path('current-user/', current_user_view, name='current-user'),
@@ -8,6 +13,10 @@ urlpatterns = [
   path('customers/<int:pk>/', CustomerUserDetailView.as_view(), name='customer-detail'),
   path('categories/', CategoryListView.as_view(), name='category-list'),
   path('categories/create/', CategoryCreateView.as_view(), name='category-create'),
+  path('properties/', PropertyListView.as_view(), name='property-list'),
+  path('properties/approved/', ApprovedPropertyListView.as_view(), name='approved-property-list'),
+  path('properties/create/', PropertyCreateView.as_view(), name='property-create'),
+  path('properties/<int:pk>/', PropertyDetailView.as_view(), name='property-detail'),
   path('login/', LoginView.as_view(), name='login'),
   path('logout/', LogoutView.as_view(), name='logout'),
 ]
